bc1806e787d23a4bb7016466c831b68e84065995
Isaac Grosof <isaacbg227@gmail.com>
2018-05-05 00:04:31 -0400

    New implementation

--- submissions1.py
+++ submissions1.py
@@ -6,22 +6,21 @@ def tit_for_whoops(m, t, s):
     else:
         return 'd' if all([x == 'd' for x in t[-2:]]) else 'c'
 
-
 def growing_distrust(mine, theirs, state):
-    # Start friendly.
+    # Start with trust.
     if len(mine) == 0:
+        state.append(dict(betrayals=0, trust=True))
         return 'c'
 
-    # Be nice if we've already forgiven the opponent, and they're playing nice.
-    if mine[-1] == 'c' and theirs[-1] == 'c':
-        return 'c'
+    state_info = state[0]
 
-    # Get mad if we've been nice and they've been spiteful.
-    betrayals = len([
-        pair for pair in zip(mine, theirs[1:]) if pair == ('c', 'd')
-    ])
-    forgive_chance = 0.5 ** betrayals
-    if random.random() < forgive_chance:
-        return 'c'
-    else:
-        return 'd'
+    # If we're trusting and we get betrayed, trust less.
+    if state_info['trust'] and theirs[-1] == 'd':
+        state_info['trust'] = False
+        state_info['betrayals'] += 1
+
+    # Forgive, but don't forget.
+    if random.random() < 0.5 ** state_info['betrayals']:
+        state_info['trust'] = True
+
+    return 'c' if state_info['trust'] else 'd'
